d13ec8cc97581b31c1e5da6674983bd7bc72ce12
Jose M. Gimenez-Garcia <jm.gimenez.garcia@gmail.com>
2021-03-12 17:41:44 +0100

    IFN3 complete

--- generate-ifn.sh
+++ generate-ifn.sh
@@ -10,11 +10,31 @@
 csvtk join -f Origen,Estrato tables/estratos_exs.csv tables/estratos.csv -o tables/provincias_exs.csv
 
 # Run sparql-generate queries
+./sparql-generate.sh -q ../sparql-generate/ifn/cambioespecie.rqg -i tables/cambioespecie.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/cambioespeciereg.rqg -i tables/cambioespeciereg.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/estratos.rqg -i tables/estratos.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/estratos_exs.rqg -i tables/estratos_exs.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/mayores_exs.rqg -i tables/mayores_exs.csv -v -s 100000
 ./sparql-generate.sh -q ../sparql-generate/ifn/parcelas_exs.rqg -i tables/parcelas_exs.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/parcpoly.rqg -i tables/parcpoly.csv -v
 ./sparql-generate.sh -q ../sparql-generate/ifn/pcdatosmap.rqg -i tables/pcdatosmap.csv -v
 ./sparql-generate.sh -q ../sparql-generate/ifn/pcespparc.rqg -i tables/pcespparc.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/pcmatorral.rqg -i tables/pcmatorral.csv -v
 ./sparql-generate.sh -q ../sparql-generate/ifn/pcmayores.rqg -i tables/pcmayores.csv -s 100000 -v
+./sparql-generate.sh -q ../sparql-generate/ifn/pcnueesp.rqg -i tables/pcnueesp.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/pcparcelas.rqg -i tables/pcparcelas.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifn/pcregenera.rqg -i tables/pcregenera.csv -v
 ./sparql-generate.sh -q ../sparql-generate/ifn/provincias_exs.rqg -i tables/provincias_exs.csv -v
+
+
+
+
+
+
+
+
+
+
 
 # Create positions for WGS84
 java -Xmx15000M -jar /usr/local/lib/epsgrdf-1.0-SNAPSHOT-jar-with-dependencies.jar tables/pcdatosmap.ttl tables/pcespparc.ttl tables/pcmayores.ttl ../Ontologies/epsg/Coordinate_Reference_System.ttl
