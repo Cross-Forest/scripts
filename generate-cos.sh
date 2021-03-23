989a014a3dc2ec67a5039cc37227c7a03d0fce4b
Jose M. Gimenez-Garcia <jm.gimenez.garcia@gmail.com>
2021-03-23 23:18:36 +0100

    Fixed script to generate COS

--- generate-cos.sh
+++ generate-cos.sh
@@ -1,12 +1,10 @@
 # #!/bin/bash
 
-./extractMFE50.sh
+./transformCOS.sh "$1/*.shp"
 
-./transformMFE50.sh "$1/*.shp"
-
-./sparql-generate.sh -q ../sparql-generate/mfe/cos_original.rqg -i "$1/.shp.json" -v
-./sparql-generate.sh -q ../sparql-generate/mfe/cos_simplified.rqg -i "$1/shp_s5_b.json" -v
-./sparql-generate.sh -q ../sparql-generate/mfe/cos_merged.rqg -i "$1/*shp_s5_s5_b_d_j_s5_b_c.json" -v
+./sparql-generate.sh -q ../sparql-generate/cos/cos_original.rqg -i "$1/.shp.json" -v
+./sparql-generate.sh -q ../sparql-generate/cos/cos_simplified.rqg -i "$1/shp_s5_b.json" -v
+./sparql-generate.sh -q ../sparql-generate/cos/cos_merged.rqg -i "$1/*shp_s5_s5_b_d_j_s5_b_c.json" -v
 
 sed -i 's/(((/((/g' $1/*.ttl
 sed -i 's/)))/))/g' $1/*.ttl
