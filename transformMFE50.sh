b6d4818951701b691644dcdddd0c4ffdd41656f3
Jose M. Gimenez-Garcia <jm.gimenez.garcia@gmail.com>
2021-03-23 09:32:04 +0100

    Now creating json for original layer

--- transformMFE50.sh
+++ transformMFE50.sh
@@ -11,6 +11,7 @@ do
   municipalities=${json%.json}_m.json
   species=${municipalities%.json}_sp.json
   clean=${species%.json}_r.json
+  box0=${clean%.json}_b.json
   simplified=${clean%.json}_s$s.json
   box=${simplified%.json}_b.json
   dissolved=${box%.json}_d.json
@@ -30,9 +31,11 @@ do
   mapshaper $municipalities -each 'if(SP3>0){SPECIES=[[SP1,O1*10,E1],[SP2,O2*10,E2],[SP3,O3*10,E3]]} else if(SP2>0){SPECIES=[[SP1,O1*10,E1],[SP2,O2*10,E2]]} else if(SP1>0){SPECIES=[[SP1,O1*10,E1]]}' -drop fields="SP1,SP2,SP3,O1,O2,O3,E1,E2,E3,TSP1,TSP2,TSP3" -o $species
   echo "*** Removing empty fields ***"
   mapshaper $species -each 'if (this.properties != null) {for (var property in this.properties) { if(this.properties[property] === "" || this.properties[property] === null){delete this.properties[property]}}}'  -o $clean
+  echo "*** Adding boundig box to original layer ***"
+  mapshaper $clean -each 'BBOX=this.bounds' -o $box0
   echo "*** Simplifying original layer ***"
   mapshaper $clean -simplify $s% -clean -o $simplified
-  echo "*** Adding boundig box to original layer ***"
+  echo "*** Adding boundig box to simplified layer ***"
   mapshaper $simplified -each 'BBOX=this.bounds' -o $box
   echo "*** Aggregating polygons by use ***"
   mapshaper $box -each 'CLAS_IFN2=Math.floor(CLAS_IFN/10)' -dissolve CLAS_IFN2 -explode -each "PROV_MFE50=$province" -o $dissolved
