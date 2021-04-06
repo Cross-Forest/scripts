12fe245ee6d9fb798b00df772782761a9bd0e304
David Portolés <dportoles@gmail.com>
2021-04-06 21:42:29 +0000

    Incluido proceso relación IFN3 estrato-IFI formación dominante

--- generate-ifi.sh
+++ generate-ifi.sh
@@ -11,3 +11,4 @@ mv tables_ifi/desnuts._PT.ttl tables_ifi/desnuts_PT.ttl
 
 ./sparql-generate.sh -q ../sparql-generate/ifi/exsistencias_formdom.rqg -i tables_ifi/exsistencias_formdom.csv -v
 ./sparql-generate.sh -q ../sparql-generate/ifi/exsistenciasporha_formdom.rqg -i tables_ifi/exsistenciasporha_formdom.csv -v
+./sparql-generate.sh -q ../sparql-generate/ifi/estratos_ifn3.rqg -i tables_ifi/estratosifn3.csv -v
