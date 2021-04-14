605b8044f275b7c859f1ce8428c67a0d7d06bfa0
David Portolés <dportoles@gmail.com>
2021-04-14 23:12:23 +0200

    Fixed generate-cos

--- generate-cos.sh
+++ generate-cos.sh
@@ -2,8 +2,8 @@
 
 ./transformCOS.sh "$1/*.shp"
 
-./sparql-generate.sh -q ../sparql-generate/cos/cos_original.rqg -i "$1/.shp.json" -v
-./sparql-generate.sh -q ../sparql-generate/cos/cos_simplified.rqg -i "$1/shp_s5_b.json" -v
+./sparql-generate.sh -q ../sparql-generate/cos/cos_original.rqg -i "$1/*.shp.json" -v
+./sparql-generate.sh -q ../sparql-generate/cos/cos_simplified.rqg -i "$1/*shp_s5_b.json" -v
 ./sparql-generate.sh -q ../sparql-generate/cos/cos_merged.rqg -i "$1/*shp_s5_s5_b_d_j_s5_b_c.json" -v
 
 sed -i 's/(((/((/g' $1/*.ttl
