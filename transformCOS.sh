07565ae346e22f898217aa14fd2fe26e41a890cd
David Portolés Rodríguez <dportoles@gmail.com>
2021-07-18 10:51:51 +0000

    Include % of use n1 and n4 of cos merged s5s5 layer

--- transformCOS.sh
+++ transformCOS.sh
@@ -4,12 +4,12 @@ s=5 # simplification level
 
 FILES=$1
 
+DROP_FIELDS="CURRENT_AREA,CURRENT_USE,CURRENT_USE_N4,CURRENT_PCT"
 echo $FILES
 
 for f in $@
 do  
   real=$(realpath $f)
-  base=$(basename $f)
   json=${real%.json}.json
   box0=${json%.json}_b.json
   simplified=${json%.json}_s$s.json
@@ -22,10 +22,9 @@ do
 
   echo "---------------------------------"
   echo "Processing file: $f"
-  echo "Processing file json: $json"
 
   echo "*** Exporting to JSON *** $(date) ***"
-  mapshaper $f -proj +proj=longlat +datum=WGS84 +no_defs -clean -each "AREA2=this.area/10000,POLYGON=this.id" -o format=geojson precision=0.000001 $json
+  mapshaper $f -proj +proj=longlat +datum=WGS84 +no_defs -clean -each 'AREA2=this.area/10000,POLYGON=this.id' -o format=geojson precision=0.000001 $json
   echo "*** Adding bounding box to original layer *** $(date) ***"
   mapshaper $json -each 'BBOX=this.bounds' -o $box0
   echo "*** Simplifying original layer *** $(date) ***"
@@ -45,8 +44,52 @@ do
     'if (SUBPOLYGONS != null) {
       var POLYGON=this.id;
       var AREA=0;
+      var CURRENT_AREA=0;
+      var CURRENT_PCT=0;
+      var MAX_PCT=0;
+      var MAX_PCT_N4=0;
+      var N1_MAX_PCT="";
+      var N4_MAX_PCT="";
+      var LG_MAX_PCT="";
+      var PCT_N1 = {};
+      var PCT_N4 = {};
+
       for (var polygon in SUBPOLYGONS) {
         AREA+=Math.max(SUBPOLYGONS[polygon].AREA,SUBPOLYGONS[polygon].AREA2);
       }
-    }' -drop fields="proportion" -o $calculations
+
+      for (var polygon in SUBPOLYGONS) {
+        CURRENT_AREA = Math.max(SUBPOLYGONS[polygon].AREA,SUBPOLYGONS[polygon].AREA2);
+        CURRENT_PCT = CURRENT_AREA / AREA;
+
+        CURRENT_USE = SUBPOLYGONS[polygon].COS2018_n1.substr(0,1);;
+        if (PCT_N1[CURRENT_USE] == null) {
+          PCT_N1[CURRENT_USE] = 0;
+        }
+        PCT_N1[CURRENT_USE] += CURRENT_PCT; 
+
+        if (PCT_N1[CURRENT_USE] > MAX_PCT) {
+            MAX_PCT = PCT_N1[CURRENT_USE];
+            N1_MAX_PCT = CURRENT_USE; 
+        }
+
+        CURRENT_USE_N4 = SUBPOLYGONS[polygon].COS2018_n4
+        for (var i=0; i < 10; i++) {
+          CURRENT_USE_N4 = CURRENT_USE_N4.replace(".","");
+        }
+        if (PCT_N4[CURRENT_USE_N4] == null) {
+          PCT_N4[CURRENT_USE_N4] = 0;
+        }
+        PCT_N4[CURRENT_USE_N4] += CURRENT_PCT; 
+
+        if (PCT_N4[CURRENT_USE_N4] > MAX_PCT_N4) {
+            MAX_PCT_N4 = PCT_N4[CURRENT_USE_N4];
+            N4_MAX_PCT = CURRENT_USE_N4; 
+            LG_MAX_PCT = SUBPOLYGONS[polygon].COS2018_Lg;
+        }
+      }
+      COS2018_n1=N1_MAX_PCT;
+      COS2018_n4=N4_MAX_PCT;
+      COS2018_Lg=LG_MAX_PCT;
+    }' -drop fields=$DROP_FIELDS -o $calculations
 done
